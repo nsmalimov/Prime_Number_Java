544a13c3f8100ad269f6cb91f660bb3c6b2e9b27
norman000 <norman000@mail.ru>
2013-10-21 03:11:44 +0400

    commit

---
+++ src/prime_number.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ */
+public class prime_number {
+    public static void main(String[] args) {
+        int n, count;
+        n = 25;
+        for (int k = 1; k < n; k++) {
+            count = 0;
+            for (int i = 1; i< n; i++) {
+                if ((k % i) == 0) {
+                    count++;
+                }
+            }
+             if (count == 2) {
+                 System.out.println(k+" ");
+
+             }
+        }
+    }
+}
+
